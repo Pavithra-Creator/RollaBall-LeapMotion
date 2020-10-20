8c43bc0cfd05d1df993463a64debcde759b97e42
Pavithra <pavitn21@gmail.com>
2020-10-20 16:48:28 +0530

    Initial commit

---
+++ Assets/colorchange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorchange : MonoBehaviour {
+    public Material[] material;
+    Renderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "color")
+        {
+            rend.sharedMaterial = material[1];
+        }
+    }
+	
+	/*// Update is called once per frame
+	void Update () {
+		
+	}*/
+}
---
+++ Assets/pickup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+
+    public GameObject Cube;
+    public GameObject Cube1;
+    public GameObject Cube2;
+    public GameObject Cube3;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //First Cube 
+        if (collider.gameObject.tag == "player" && gameObject.tag == "cube")
+        {
+            Cube.SetActive(false);
+            Cube1.SetActive(true);
+            Cube3.SetActive(true);
+
+        }
+        //Second Cube
+        if (collider.gameObject.tag == "player" && gameObject.tag == "cube1")
+        {
+
+            Cube1.SetActive(false);
+            Cube2.SetActive(true);
+            Cube.SetActive(true);
+            Cube3.SetActive(false);
+        }
+        if (collider.gameObject.tag == "player" && gameObject.tag == "cube2")
+        {
+            Cube2.SetActive(false);
+            Cube3.SetActive(true);
+            Cube1.SetActive(true);
+            Cube.SetActive(false);
+        }
+        if (collider.gameObject.tag == "player" && gameObject.tag == "cube3")
+        {
+            Cube.SetActive(true);
+            Cube1.SetActive(true);
+            Cube2.SetActive(true);
+            Cube3.SetActive(true);
+
+
+        }
+
+
+
+    }
+
+}
